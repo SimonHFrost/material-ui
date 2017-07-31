b76d7c34ce3124251fded23f298386354e3928b6
Simon Frost <simon.frost@planet.com>
2017-07-31 13:56:56 +0200

    Trailing month test

--- src/DatePicker/dateUtils.spec.js
+++ src/DatePicker/dateUtils.spec.js
@@ -4,17 +4,30 @@ import * as Utils from './dateUtils';
 
 describe('Date Utils', () => {
   describe('getWeekArray', () => {
-    it('pads previous month entries with nulls', () => {
+    it('leading month padded with nulls', () => {
       const date = new Date(1498867200000); // Sat, 01 July 2017 00:00:00 GMT
       const weekArray = Utils.getWeekArray(date, 1);
 
       const firstWeek = weekArray[0];
 
-      // First week should have 7 days
+      // A week should have 7 days
       assert.strictEqual(firstWeek.length, 7);
 
-      // Five days are from the previous month
+      // Verify null days from leading month
       assert.strictEqual(firstWeek.filter(((day) => day === null)).length, 5);
+    });
+
+    it('trailing month padded with nulls', () => {
+      const date = new Date(1498867200000); // Sat, 01 July 2017 00:00:00 GMT
+      const weekArray = Utils.getWeekArray(date, 1);
+
+      const lastWeek = weekArray[5];
+
+      // A week should have 7 days
+      assert.strictEqual(lastWeek.length, 7);
+
+      // Verify null days from trailing month
+      assert.strictEqual(lastWeek.filter(((day) => day === null)).length, 6);
     });
   });
 });
